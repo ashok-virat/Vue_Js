226014dc6841da91a95b4275e545b109ff4cc8c2
Ashok24 <aruashok17@gmail.com>
2021-08-07 11:24:13 +0530

    dark mode removed

--- src/components/Home/HomeComponent.vue
+++ src/components/Home/HomeComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div>
-  <b-navbar toggleable type="dark">
-    <b-navbar-brand style="color:black">Blog</b-navbar-brand>
+  <b-navbar toggleable type="dark" variant="dark">
+    <b-navbar-brand class='brand'>Akz</b-navbar-brand>
 
     <b-navbar-toggle target="navbar-toggle-collapse">
       <template #default="{ expanded }">
@@ -10,7 +10,7 @@
         <b-icon v-else icon="chevron-bar-down"></b-icon>
       </template>
     </b-navbar-toggle>
-    <b-collapse id="navbar-toggle-collapse" style="color:white" is-nav>
+    <b-collapse id="navbar-toggle-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item @click='openBlog'>Post Stories</b-nav-item>
             <b-nav-item @click="logOut">Logout</b-nav-item>
@@ -39,5 +39,10 @@
 }
 .title {
   font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+.brand {
+ font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serifp;
+  color: red;
+  margin-left: 10px;
 }
 </style>
